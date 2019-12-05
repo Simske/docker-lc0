d009b44f96412b7e8b7feb5f259cf601609a8fdb
Simske <mail@simske.com>
2019-12-05 13:35:51 +0100

    Restructured dockerfile generator, added build function

--- docker/generate_dockerfile.py
+++ docker/generate_dockerfile.py
@@ -1,26 +1,43 @@
 #!/usr/bin/env python3
 
+import argparse
+# import subprocess
 import yaml
 from jinja2 import Template
+class subprocess:
+    def run(*args, **kwargs):
+        print(args, kwargs)
 
 with open("config.yml") as f:
     config = yaml.safe_load(f)
 
-with open("Dockerfile.template") as f:
-    dockerfile = Template(f.read(), trim_blocks=True)
-
-def get_config(profilename):
+def get_profile(profilename):
     if 'base' in config[profilename]:
-        conf = get_config(config[profilename]['base']).copy()
+        conf = get_profile(config[profilename]['base']).copy()
         conf.update(config[profilename])
         return conf
     else:
         return config[profilename]
 
+def generate_dockerfiles():
+    with open("Dockerfile.template") as f:
+        dockerfile = Template(f.read(), trim_blocks=True)
 
+    for profilename, profile in config.items():
+        conf = get_profile(profilename)
 
-for profilename, profile in config.items():
-    conf = get_config(profilename)
+        with open(f"dockerfiles/Dockerfile.{profilename}", 'w') as f:
+            f.write(dockerfile.render(**conf))
 
-    with open(f"dockerfiles/Dockerfile.{profilename}", 'w') as f:
-        f.write(dockerfile.render(**conf))
+def build_image(profilename):
+    profile = get_profile(profilename)
+    imagename = profile['imagename']
+    tag = f"{profile['lc0_version']}{profile['tag_suffix']}"
+    subprocess.run(['docker', 'build', '-t', f"{imagename}:{tag}", '-f', f'dockerfiles/Dockerfile.{profilename}', '.'], check=True)
+    return tag
+
+if __name__ == "__main__":
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument("-v", "--version", type=str, help="lc0 version")
+    # args = parser.parse_args()
+    build_image('default')
